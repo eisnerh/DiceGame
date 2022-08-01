9edf1f12601b1003a0210c61b3d7a181407626be
Eisner López Acevedo <eisner.acevedo@forsyteit.com>
2022-07-31 19:50:10 -0600

    Primer Commit

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -11,13 +11,18 @@ namespace DiceGame
         static void Main(string[] args)
         {
             int playerRandomNum;
+            int enemyRandomNum;
             Random random = new Random();
             for(int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Press any key to roll the dice. ");
                 playerRandomNum = random.Next(1, 7);
-                Console.ReadKey();
                 Console.WriteLine("You rolled a " + playerRandomNum);
+                Console.WriteLine(".......");
+                System.Threading.Thread.Sleep(1000);
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
+                Console.ReadKey();
             }
         }
     }
