0fb5a42aabb6f143688bdd08d76499f3d4b14298
Eisner López Acevedo <eisner.acevedo@forsyteit.com>
2022-07-31 20:03:07 -0600

    Adding Scores

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -12,6 +12,8 @@ namespace DiceGame
         {
             int playerRandomNum;
             int enemyRandomNum;
+            int playerPoints = 0;
+            int enemyPoints = 0;
             Random random = new Random();
             for(int i = 0; i < 10; i++)
             {
@@ -22,6 +24,29 @@ namespace DiceGame
                 System.Threading.Thread.Sleep(1000);
                 enemyRandomNum = random.Next(1, 7);
                 Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("Player wins this round! ");
+                }
+                else if (enemyRandomNum > playerRandomNum)
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy wins this round! ");
+                } else
+                {
+                    Console.WriteLine("Draw");
+                }
+                Console.WriteLine("The Score now is - Player : " + playerPoints);
+                Console.WriteLine("The Score now is - Enemy : " + enemyPoints);
+                
+            }
+            if (playerPoints>enemyPoints) 
+            {
+                Console.WriteLine("Player Won!!!");
+            } else
+            {
+                Console.WriteLine("Enemy Won!!!");
             }
             Console.ReadKey();
         }
